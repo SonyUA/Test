1dbcb8974cebdfcb3d50f3a494b12b3363fd2262
SonyUa <pankivluck@gmail.com>
2019-10-14 14:00:16 +0300

    efects

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
 'use strict'
+/* SLIDER */
 let images = document.querySelectorAll(".slide__block-card");
 const arrowLeft = document.querySelector("#arrow-left");
 const arrowRight = document.querySelector("#arrow-right");
@@ -36,3 +37,31 @@ function slide() {
     arrowRight.addEventListener('click', right);
 }
 slide();
+/* MENU */
+const menu = document.querySelector('.social__menu');
+let shareItLink = document.querySelector('.produkt__link');
+let cross = document.querySelector('.social__list-close');
+
+function showMenu() {
+    menu.classList.toggle("social__menu-show");
+}
+shareItLink.addEventListener('click', showMenu);
+cross.addEventListener('click', showMenu);
+/* STARS RATING */
+let starsBlock = document.querySelector('.stars__block');
+let stars = document.querySelectorAll('.produkt__star');
+
+function starsActive(event) {
+    let target = event.target;
+    let countStars = 4
+    for (let i = 0; i < stars.length; i++) {
+        console.log(parseInt(target.dataset.rate))
+        if (i <= parseInt(target.dataset.rate)) {
+            stars[i].classList.add('produkt__star-red')
+        } else {
+            stars[i].classList.remove('produkt__star-red')
+        }
+    }
+}
+starsBlock.addEventListener('click', starsActive);
+starsBlock.addEventListener('mouseover', starsActive);
